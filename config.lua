f12f784799616594a634d7f79248b1e14db9f2ff
ciabalaiz <139115109+ciabalaiz@users.noreply.github.com>
2024-08-21 17:27:35 +0200

    Added config

---
+++ config.lua
@@ -0,0 +1,16 @@
+local config =  
+	
+{
+	damage = 50;
+	headshot = 100;
+	firing = 1.1;
+	rpm = 700;
+	magCapacity = 30;
+	maxAmmo = 20;
+	velocity = 50;
+	fireRate = .1;
+	range = 5000;
+}
+
+
+return config
